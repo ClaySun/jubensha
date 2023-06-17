ecf1d1dec52829f974eb38a9636d69910dbdfbae
Gray Sun <skl_ahut@163.com>
2023-06-17 19:43:51 +0800

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,20 +13,26 @@
 
 <style scoped>
   .home-bg {
-    position: relative;
-    width: 100vw;
-    height: 100vh;
+    z-index: 1;
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
     background-image: url(../assets/images/home-bg.png);
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center center;
   }
   .home-link {
-    position: absolute;
+    z-index: 2;
+    position: fixed;
     top: 0;
-    left: 0;
-    bottom: 0;
     right: 0;
+    bottom: 0;
+    left: 0;
     width: 100%;
     height: 100%;
     background-image: url(../assets/images/home-btn.png);
