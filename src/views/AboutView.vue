6335c4b4c28cbc853aa7d80fb65c1c070ca167f0
Gray Sun <skl_ahut@163.com>
2023-06-17 16:07:54 +0800

    about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,98 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about-bg">
+    <div class="desc-bg">
+      <div class="desc-wrapper">
+        <img class="" :src="desc" alt="desc"/>
+        <div class="select-btn"></div>
+        <div class="tag"></div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<script>
+import desc from '../assets/images/desc.png'
+export default {
+  data () {
+    return {
+      desc
+    }
   }
+}
+</script>
+
+<style>
+::-webkit-scrollbar{display:none}
+.about-bg {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: red;
+  background-image: url(../assets/images/about-bg.png);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+}
+.desc-bg {
+  position: relative;
+  /* box-sizing: border-box; */
+  width: 90vw;
+  height: 96vh;
+  /* padding: 14vh 10vw 14vh 10vw; */
+  background-image: url(../assets/images/about-desc-bg.png);
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center center;
+}
+.select-btn {
+  z-index: 2;
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  margin-left: -110px;
+  width: 4.18rem;
+  height: 1.7rem;
+  background-image: url(../assets/images/select-btn.png);
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center center;
+}
+.tag {
+  z-index: 1;
+  position: absolute;
+  bottom: -1.2rem;
+  right: 50%;
+  margin-right: -2.4rem;
+  width: 1rem;
+  height: 2rem;
+  background-image: url(../assets/images/tag.png);
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center center;
+}
+.desc-wrapper {
+  margin: 0 auto;
+  margin-top: 2.8rem;
+  overflow: scroll;
+  width: 5.8rem;
+  height: 11rem;
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+}
+
+.desc-wrapper>img {
+  display: block;
+  width: 100%;
+}
+.desc-title {
+  /* margin-bottom: 2vh;
+  text-align: center;
+  font-size: 18px; */
+}
+.desc {
+  text-indent: 2em;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
+
+<template>
+  <div class="home-bg">
+    <router-link to="/about">
+      <div class="home-link"></div>
+    </router-link>
+    
+  </div>
+</template>
+
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
-<template>
-  <main>
-    home
-  </main>
-</template>
+<style scoped>
+  .home-bg {
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    background-color: red;
+    background-image: url(../assets/images/home-bg.png);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+  }
+  .home-link {
+    position: absolute;
+    top: 0;
+    left: 80px;
+    width: 107px;
+    height: 415px;
+    background-image: url(../assets/images/home-btn.png);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+  }
+</style>
