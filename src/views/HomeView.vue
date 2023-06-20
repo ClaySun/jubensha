b3a4e7cc728ac2f41fb5492d032a33a1d73f6574
Gray Sun <skl_ahut@163.com>
2023-06-20 21:26:40 +0800

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,15 +14,15 @@
 <style scoped>
   .home-bg {
     z-index: 1;
-    position: fixed;
+    position: absolute;
     top: 0;
     right: 0;
     bottom: 0;
     left: 0;
     width: 100%;
     height: 100%;
-    padding-top: env(safe-area-inset-top);
-    padding-bottom: env(safe-area-inset-bottom);
+    /* padding-top: env(safe-area-inset-top);
+    padding-bottom: env(safe-area-inset-bottom); */
     background-image: url(../assets/images/home-bg.png);
     background-size: cover;
     background-repeat: no-repeat;
@@ -30,15 +30,15 @@
   }
   .home-link {
     z-index: 2;
-    position: fixed;
+    position: absolute;
     top: 0;
     right: 0;
     bottom: 0;
     left: 0;
     width: 100%;
     height: 100%;
-    padding-top: env(safe-area-inset-top);
-    padding-bottom: env(safe-area-inset-bottom);
+    /* padding-top: env(safe-area-inset-top);
+    padding-bottom: env(safe-area-inset-bottom); */
     background-image: url(../assets/images/home-btn.png);
     background-size: cover;
     background-repeat: no-repeat;
